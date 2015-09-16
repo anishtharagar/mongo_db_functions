c0cc84e67b77c1aa61733e96f3d4834dc8455677
Anish <anish.tharagar@gmail.com>
2015-09-17 01:53:09 +0530

    enhancement related to shaping of data loading onto mongodb added to the source code, you can now uplload data in split mode and join mode; split mode is where you pass a dictionary and we split it into multiple documents each key in the dict will have one document in the mongo database; join mode is where we join all the keys in the dict and create a single document in mongodb

--- mongo_connect.py
+++ mongo_connect.py
@@ -2,21 +2,25 @@
 
 
 import pymongo
+import sys
 
 
-def mongo_load(mongo_db_host,mongo_db_name,mongo_collection_name,keys = [],data_payload = []):
+def mongo_load(mongo_db_host,mongo_db_name,mongo_collection_name,align_type,data_payload = {}):
 
     try:
             mongo_db_conn = pymongo.Connection (mongo_db_host)
             mongo_db_conn_cursor = mongo_db_conn [mongo_db_name]
             mongo_collections_cursor = mongo_db_conn_cursor [mongo_collection_name]
-            data_load={}
-
-            for i in range(len(data_payload)):
-                data_load.__setitem__(keys.__getitem__(i),data_payload.__getitem__(i))
-
-            mongo_collections_cursor.insert(data_load)
+            if align_type == 'split':
+                for key,values in data_payload.items():
+                    loader_data = {key: values}
+                    mongo_collections_cursor.insert(loader_data)
+            elif align_type == 'join':
+                mongo_collections_cursor.insert(data_payload)
+            else:
+                raise ValueError ('kindly specify value for align type')
             mongo_db_conn.close()
+
     except OSError as e:
         print "OS Error({0}): {1}", format(e.errno, e.strerror)
     except IOError as e:
@@ -44,3 +48,26 @@ def mongo_get(mongo_db_host,mongo_db_name,mongo_collection_name,mongo_query):
         print "Unexpected error:", sys.exc_info()[0]
 
 
+#### Function to delete the collection and load new data into the collection which was deleted.
+
+def mongo_delete_load(mongo_db_host_del,mongo_db_name_del,mongo_collection_name_del,align_type_del,data_payload_del = []):
+
+    try:
+        mongo_db_conn_del = pymongo.Connection (mongo_db_host_del)
+        mongo_db_conn_cursor_del = mongo_db_conn_del [mongo_db_name_del]
+        mongo_collections_cursor_del = mongo_db_conn_cursor_del [mongo_collection_name_del]
+        mongo_collections_cursor_del.remove()
+        mongo_db_conn_del.close()
+
+        mongo_load(mongo_db_host_del,mongo_db_name_del,mongo_collection_name_del,align_type_del,data_payload_del)
+
+    except OSError as e:
+        print "OS Error({0}): {1}", format(e.errno, e.strerror)
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    except  ValueError:
+        print "Could not convert data to an integer."
+    except  SystemError as e:
+        print "System Error({0})", format(e.message)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
